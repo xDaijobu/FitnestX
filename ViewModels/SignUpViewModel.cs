89a5547c6a1fe2ac027d8bd4ff59c7f98fc0ea6d
Cristover Wurangian <christwurangian@gmail.com>
2023-02-02 10:40:10 +0700

    updated checkbox terms

--- ViewModels/SignUpViewModel.cs
+++ ViewModels/SignUpViewModel.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace FitnestX.ViewModels
 {
     [INotifyPropertyChanged]
     public partial class SignUpViewModel
     {
+
+        [ObservableProperty] 
+        private bool isTermsChecked;
+
+        [RelayCommand]
+        private void TermsClicked()
+        {
+            IsTermsChecked = !IsTermsChecked;
+        }
     }
 }
