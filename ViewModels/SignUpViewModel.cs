291a7573ce4ef5db76957f92eba37f89741fde47
Cristover Wurangian <christwurangian@gmail.com>
2023-02-02 16:03:18 +0700

    upd SignUp

--- ViewModels/SignUpViewModel.cs
+++ ViewModels/SignUpViewModel.cs
@@ -1,24 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
-namespace FitnestX.ViewModels
+namespace FitnestX.ViewModels;
+
+[INotifyPropertyChanged]
+public partial class SignUpViewModel
 {
-    [INotifyPropertyChanged]
-    public partial class SignUpViewModel
+
+    [ObservableProperty]
+    private bool isTermsChecked;
+
+    [ObservableProperty] 
+    private bool canStateChange;
+
+    [ObservableProperty] 
+    private string currentState = States.SignUpStepTwo;
+
+    [RelayCommand]
+    private void TermsClicked()
     {
-
-        [ObservableProperty] 
-        private bool isTermsChecked;
-
-        [RelayCommand]
-        private void TermsClicked()
-        {
-            IsTermsChecked = !IsTermsChecked;
-        }
+        IsTermsChecked = !IsTermsChecked;
     }
+
+    [RelayCommand(CanExecute = nameof(CanStateChange))]
+    private void ChangeState()
+    {
+        CurrentState = States.SignUpStepTwo;
+    }
+
+    partial void OnCanStateChangeChanged(bool value)
+    {
+        ChangeStateCommand.NotifyCanExecuteChanged();
+    }
+}
+
+static class States
+{
+    public const string SignUpStepOne = nameof(SignUpStepOne);
+    public const string SignUpStepTwo = nameof(SignUpStepTwo);
+    public const string GoalStepOne = nameof(GoalStepOne);
+    public const string GoalStepTwo = nameof(GoalStepTwo);
+    public const string GoalStepThree = nameof(GoalStepThree);
 }
